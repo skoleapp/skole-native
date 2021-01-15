824c5231c65cc30771b0ff8a3e906937e28d67d4
Markus Blomqvist <markus@skoleapp.com>
2021-01-15 14:33:12 +0200

    Use the same eslint and prettier configuration as in the frontend repo

--- App.tsx
+++ App.tsx
@@ -2,8 +2,5 @@ import React from 'react';
 import WebView from 'react-native-webview';
 
 export const App = () => (
-  <WebView
-    userAgent="skole-native-app"
-    source={{uri: 'https://www.skoleapp.com'}}
-  />
+  <WebView userAgent="skole-native-app" source={{ uri: 'https://www.skoleapp.com' }} />
 );
